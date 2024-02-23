923c2defd48cc82a04e3bdce80a636ca67ea0836
Michele Colella <michelecolella0@gmail.com>
2024-02-23 10:12:27 +0100

    Initial Commit

---
+++ BeaconTest/BeaconTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconTestApp.swift
+//  BeaconTest
+//
+//  Created by Michele Colella on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BeaconTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
